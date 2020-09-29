1942412349e5e5bf8e70fc0dc16d705818f61bb2
skyzen <piotr.ultimo@gmail.com>
2020-09-29 18:39:35 +0200

    OOP Clean-up

--- Flask-RESTful-SQLAlchemy/models/item.py
+++ Flask-RESTful-SQLAlchemy/models/item.py
@@ -8,3 +8,36 @@ class ItemModel:
 
     def json(self):
         return {'name': self.name, 'price': self.price}
+
+    @classmethod
+    def find_by_name(cls, name):
+        connection = sqlite3.connect('../code/data.db')
+        cursor = connection.cursor()
+
+        query = "SELECT * FROM items WHERE name=?"
+        result = cursor.execute(query, (name,))
+        row = result.fetchone()
+        connection.close()
+
+        if row:
+            return cls(*row)
+
+    def insert(self):
+        connection = sqlite3.connect('../code/data.db')
+        cursor = connection.cursor()
+
+        query = "INSERT INTO items VALUES (?, ?)"
+        cursor.execute(query, (self.name, self.price))
+
+        connection.commit()
+        connection.close()
+
+    def update(self):
+        connection = sqlite3.connect('../code/data.db')
+        cursor = connection.cursor()
+
+        query = "UPDATE items SET price=? WHERE name=?;"
+        cursor.execute(query, (self.price, self.name))
+
+        connection.commit()
+        connection.close()
